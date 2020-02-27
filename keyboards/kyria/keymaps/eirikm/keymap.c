7fd8433f9112ab70a9ef36cd7e43530c5feddfc6
Eirik Meland <eirik.meland@arktekk.no>
2020-02-27 21:13:46 +0100

    debug

--- keyboards/kyria/keymaps/eirikm/keymap.c
+++ keyboards/kyria/keymaps/eirikm/keymap.c
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                                           KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_QUOT,
      CTRL_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                                           KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_ENT,
-      KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,    KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,
-                                 KC_MUTE, KC_LALT, KC_LCMD, KC_BSPC,MO(_SYM),   MO(_NAV),  KC_SPC,MO(_NUM), XXXXXXX, XXXXXXX
+      KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_1,    KC_2,       KC_3,    KC_4,    KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,
+                                 KC_MUTE, KC_LALT, KC_LCMD, KC_BSPC,    KC_5,   MO(_NAV),  KC_SPC,MO(_NUM), XXXXXXX, XXXXXXX
     ),
 /*
  * Lower Layer: Symbols
